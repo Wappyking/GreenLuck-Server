f321970e9136e9336b0e32d30b9e82f73e80ef25
Wappyking <Wappyfresh@gmail.com>
2025-03-06 00:32:45 +0100

    14th commit

--- src/plan/plan-model.js
+++ src/plan/plan-model.js
@@ -1,11 +1,11 @@
 const supabase = require("../../config/supaconfig_index");
 
 function fetch_all_plans_model() {
-  return supabase.from("free").select("*");
+  return supabase.from("plan").select("*");
 }
 
 function fetch_plans_id_model(payload) {
-  return supabase.from("free").select("*").eq("id", payload);
+  return supabase.from("plan").select("*").eq("id", payload);
 }
 
 module.exports = { fetch_all_plans_model, fetch_plans_id_model };
