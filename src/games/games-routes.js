84798fcf3ef90ddf81c2902e85c79ccbff903c39
Wappyking <Wappyfresh@gmail.com>
2025-03-05 15:49:53 +0100

    12th commit

--- src/games/games-routes.js
+++ src/games/games-routes.js
@@ -17,12 +17,12 @@ const routes = Router();
 routes.post("/post-free-games", PostFreeGames);
 routes.get("/fetch-free-games", FetchFreeGames);
 routes.post("/fetch-free-code", FetchFreeID);
-routes.post("/fetch-free-games_list", FetchFreeGamesList);
+routes.post("/fetch-free-games-list", FetchFreeGamesList);
 routes.post("/delete-free-games", DeleteFreeGames);
 routes.post("/post-premium-games", PostPremiumGames);
 routes.get("/fetch-premium-games", FetchPremiumGames);
 routes.post("/fetch-premium-code", FetchPremiumID);
-routes.post("/fetch-premium-games_list", FetchPremiumGamesList);
+routes.post("/fetch-premium-games-list", FetchPremiumGamesList);
 routes.post("/delete-premium-games", DeletePremiumGames);
 
 module.exports = routes;
