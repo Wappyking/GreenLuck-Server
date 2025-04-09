940d18eb898ef1b428cb11f82efc453908084c3e
Wappyking <Wappyfresh@gmail.com>
2025-04-09 17:17:10 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
+const cors = require("cors");
 const port = 3000;
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
