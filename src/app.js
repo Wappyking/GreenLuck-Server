7cf9652ab94d8682241d51946abee9b80add89c6
Wappyking <Wappyfresh@gmail.com>
2025-04-10 19:09:06 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -4,11 +4,29 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(cors());
 app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1", cors(), require("./routes/index"));
+var whitelist = [
+  "http://greenlucktips.com",
+  "https://greenlucktips.com",
+  "http://localhost:3000",
+  "http://localhost.com:3000",
+  "http://192.168.1.122:3000",
+];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+app.use(cors(corsOptionsDelegate));
+
+app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
 //     res.send(req.body);
 //   });
