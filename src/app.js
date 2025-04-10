fefa672da88d6c4c821c2316b2af40f062bc28e4
Wappyking <Wappyfresh@gmail.com>
2025-04-10 14:34:26 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -6,24 +6,26 @@ const app = express();
 const port = 3000;
 // app.use(express.json());
 
-const allowedOrigins = [
-  "http://localhost.com:3000",
-  "https://greenlucktips.com",
-  "https://192.168.1.122:3000",
-];
+// const allowedOrigins = [
+//   "http://localhost.com:3001",
+//   "https://greenlucktips.com",
+//   "https://192.168.1.122:3000",
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-};
-app.use(cors(corsOptions));
+// const corsOptions = { origin: "http://localhost.com:3001" };
+
+//  {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg =
+//         "The CORS policy for this site does not allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+// };
+app.use(cors());
 
 app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
