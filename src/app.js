053b468ad2bf460f3e11ff7e9e1728e490ca175a
Wappyking <Wappyfresh@gmail.com>
2025-04-10 09:50:14 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
-const nodemailer = require("nodemailer");
-const app = express();
 const cors = require("cors");
+
+// const nodemailer = require("nodemailer");
+const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = ["https://localhost.com", "https://greenlucktips.com"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+};
+app.use(cors(corsOptions));
 
 app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
