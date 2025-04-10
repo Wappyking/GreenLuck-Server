ca663c676ec86587068cae64c4c25f0c7b054ba1
Wappyking <Wappyfresh@gmail.com>
2025-04-10 14:52:13 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -4,28 +4,33 @@ const cors = require("cors");
 // const nodemailer = require("nodemailer");
 const app = express();
 const port = 3000;
-// app.use(express.json());
+app.use(express.json());
 
-// const allowedOrigins = [
-//   "http://localhost.com:3001",
-//   "https://greenlucktips.com",
-//   "https://192.168.1.122:3000",
-// ];
+const allowedOrigins = [
+  "http://localhost:3001",
+  "https://greenlucktips.com",
+  "https://192.168.1.122:3000",
+];
 
-// const corsOptions = { origin: "http://localhost.com:3001" };
-
-//  {
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg =
-//         "The CORS policy for this site does not allow access from the specified Origin.";
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-// };
-app.use(cors());
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg =
+        "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders:
+    "Content-Type,Authorization,X-Requested-With,Accept,application/json",
+  credentials: true,
+  optionsSuccessStatus: 204,
+  preflightContinue: false, // Pass the OPTIONS request to the next handler (rarely needed)
+  maxAge: 3600, // Cache preflight response for 1 hour
+};
+app.use(cors(corsOptions));
 
 app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
