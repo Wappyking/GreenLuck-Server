ce0b55056c6643dfd2156977263ed6bae2c4d7fc
Wappyking <Wappyfresh@gmail.com>
2025-04-11 19:24:40 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -24,12 +24,29 @@ app.options(
 );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  const allowedOrigins = [
+    "https://your-frontend-app.com",
+    "http://localhost:3000",
+  ];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Handle Preflight Request
+  if (req.method === "OPTIONS") {
+    // Preflight requests should not reach your route handlers
+    return res.sendStatus(204); // Respond with 204 No Content
+  }
+
   next();
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require("express");
 const routes = Router();
 
 routes.use("/auth", require("../auth/auth-routes"));
-
 routes.use("/user", require("../user/user-routes"));
 routes.use("/games", require("../games/games-routes"));
 routes.use("/plan", require("../plan/plan-routes"));
