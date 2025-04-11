1d427b92f4cd40e88016939b6c88dc6a4fd4e8e4
Wappyking <Wappyfresh@gmail.com>
2025-04-11 17:42:08 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -8,41 +8,41 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
 // Define allowed origins
-const allowedOrigins = [
-  "https://greenlucktips.com",
-  "https://www.greenlucktips.com",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://192.168.1.122:3000",
-  "http://192.168.1.122:3001",
-];
+// const allowedOrigins = [
+//   "https://greenlucktips.com",
+//   "https://www.greenlucktips.com",
+//   "http://localhost:3000",
+//   "http://localhost:3001",
+//   "http://192.168.1.122:3000",
+//   "http://192.168.1.122:3001",
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
 
-    if (allowedOrigins.indexOf(origin) === -1) {
-      console.warn(`CORS: Blocked origin ${origin}`); // Log blocked origins
-      const msg =
-        "The CORS policy for this site does not allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true); // Allow the origin
-  },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
-  allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization", // Allowed headers
-  credentials: true, // Allow cookies/auth headers
-  optionsSuccessStatus: 204, // Respond with 204 for preflight requests
-  maxAge: 86400, // Cache preflight for 1 day (in seconds)
-};
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       console.warn(`CORS: Blocked origin ${origin}`); // Log blocked origins
+//       const msg =
+//         "The CORS policy for this site does not allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true); // Allow the origin
+//   },
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
+//   allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization", // Allowed headers
+//   credentials: true, // Allow cookies/auth headers
+//   optionsSuccessStatus: 204, // Respond with 204 for preflight requests
+//   maxAge: 86400, // Cache preflight for 1 day (in seconds)
+// };
 
 // Apply CORS middleware globally or to specific routes
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Optionally enable pre-flight across-the-board
 // Useful if you have complex routing and want OPTIONS handled early
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
   const allowedOrigins = [
