d41077c3a3181d3cc46daa443db29b8ae9b63731
Wappyking <Wappyfresh@gmail.com>
2025-04-11 18:55:43 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -4,47 +4,32 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.use(function (req, res, next) {
-  var oneof = false;
-  if (req.headers.origin) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    oneof = true;
-  }
-  if (req.headers["access-control-request-method"]) {
-    res.header(
-      "Access-Control-Allow-Methods",
-      req.headers["access-control-request-method"]
-    );
-    oneof = true;
-  }
-  if (req.headers["access-control-request-headers"]) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      req.headers["access-control-request-headers"]
-    );
-    oneof = true;
-  }
-  if (oneof) {
-    res.header("Access-Control-Max-Age", 60 * 60 * 24 * 365);
-  }
+// enable cors
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+app.options(
+  "*",
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 
-  // intercept OPTIONS method
-  if (oneof && req.method == "OPTIONS") {
-    res.send(200);
-  } else {
-    next();
-  }
-});
-
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
