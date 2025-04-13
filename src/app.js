9e194779a686ae1e5f6171935b4cf178e8c8708f
Wappyking <Wappyfresh@gmail.com>
2025-04-13 22:03:40 +0100

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,53 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-// const nodemailer = require("nodemailer");
 const app = express();
-const port = 3000;
-// app.use(express.json());
+const bodyParser = require("body-parser");
 
-// Define allowed origins
-// const allowedOrigins = [
-//   "https://greenlucktips.com",
-//   "https://www.greenlucktips.com",
-//   "http://localhost:3000",
-//   "http://192.168.1.122:3000",
-// ];
+// port
+let port = 1313;
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // Allow requests with no origin (like mobile apps or curl requests)
-//     if (!origin) return callback(null, true);
+// this package helps us recieve data from users in json format durring post method
+app.use(bodyParser.json()); //application json
 
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       console.warn(`CORS: Blocked origin ${origin}`); // Log blocked origins
-//       const msg =
-//         "The CORS policy for this site does not allow access from the specified Origin.";
-//       // return callback(new Error(msg), false);
-//       return callback(null, true); // Allow the origin
-//     }
-//     return callback(null, true); // Allow the origin
-//   },
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
-//   allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization", // Allowed headers
-//   credentials: true, // Allow cookies/auth headers
-//   optionsSuccessStatus: 204, // Respond with 204 for preflight requests
-//   maxAge: 86400, // Cache preflight for 1 day (in seconds)
-// };
+// for post requests
+app.use(express.json());
 
-// Apply CORS middleware globally or to specific routes
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    // preflightContinue: true,
-    // optionsSuccessStatus: 200,
-    // allowedHeaders: "*",
-  })
-);
-
-// Optionally enable pre-flight across-the-board
-// Useful if you have complex routing and want OPTIONS handled early
-// app.options("*", cors(corsOptions));
+// this middleware allows CORS (cross origin resource sharing)
+// which means api can be shared between different servers running
+// on different ports,
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // this can be predictor.com
+  res.setHeader("Access-Control-Allow-Method", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/api/v1", require("./routes/index"));
 // app.get("/", (req, res) => {
