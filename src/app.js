e9deb42e1c44b395f13783d58b637348f6606b85
Wappyking <Wappyfresh@gmail.com>
2025-04-16 17:34:38 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,35 +1,51 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+let express = require("express");
+// require("dotenv").config();
+
+// require rxpress path module
+const path = require("path");
+const http = require("http");
+// importing body parser
 const bodyParser = require("body-parser");
 
+// const { initializeSocket, getIo } = require("./utilities/io");
+// const { Port } = require("../config/supabase-auth");
+
 // port
-let port = 3323;
+// let port = Port || 9090;
+let port = 1111;
+
+let app = express();
+
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+// initializeSocket(server);
 
 // this package helps us recieve data from users in json format durring post method
-// app.use(bodyParser.json());
-//application json
+// app.use(bodyParser.json({ limit: "10mb" })); //application json
 
 // for post requests
 app.use(express.json());
 
-// app.use(cors());
+app.use("/assets/images", express.static(path.join(__dirname, "images")));
 
-// this middleware allows CORS (cross origin resource sharing)
-// which means api can be shared between different servers running
-// on different ports,
+app.use(express.static(path.join(__dirname, "images")));
+
 app.use((req, res, next) => {
+  // req.IO = getIo();
   res.setHeader("Access-Control-Allow-Origin", "*"); // this can be predictor.com
   res.setHeader("Access-Control-Allow-Method", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-app.use("/api/v1", require("./routes/index"));
-app.get("/", (req, res) => {
-  res.send("We are good");
+// Routes
+// app.use("/API/V1", require("./"))
+// app.use("/APP", require("./APP/routes/index"))
+app.use("/", (req, res) => {
+  res.send("Welcome to pocket voucher server");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+server.listen(process.env.PORT || 9090, () => {
+  console.log("Server started on port " + 9090);
 });
